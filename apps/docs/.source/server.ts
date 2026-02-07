c880a586f27c67ed6466a345d55add9526bae5a1
haZya <63403456+haZya@users.noreply.github.com>
2026-02-07 17:54:57 +0530

    docs: improve structure

--- apps/docs/.source/server.ts
+++ apps/docs/.source/server.ts
@@ -1,16 +1,17 @@
 // @ts-nocheck
-import * as __fd_glob_22 from "../content/docs/guides/troubleshooting.mdx?collection=docs"
-import * as __fd_glob_21 from "../content/docs/guides/text-vs-children.mdx?collection=docs"
-import * as __fd_glob_20 from "../content/docs/guides/react-use-shine.mdx?collection=docs"
-import * as __fd_glob_19 from "../content/docs/guides/performance-and-lifecycle.mdx?collection=docs"
-import * as __fd_glob_18 from "../content/docs/guides/light-position-and-intensity.mdx?collection=docs"
-import * as __fd_glob_17 from "../content/docs/guides/dynamic-updates.mdx?collection=docs"
-import * as __fd_glob_16 from "../content/docs/guides/class-api.mdx?collection=docs"
-import * as __fd_glob_15 from "../content/docs/getting-started/vanilla-quick-start.mdx?collection=docs"
-import * as __fd_glob_14 from "../content/docs/getting-started/react-quick-start.mdx?collection=docs"
-import * as __fd_glob_13 from "../content/docs/getting-started/nextjs-client-components.mdx?collection=docs"
-import * as __fd_glob_12 from "../content/docs/getting-started/installation.mdx?collection=docs"
-import * as __fd_glob_11 from "../content/docs/examples/shadow-rgb-cookbook.mdx?collection=docs"
+import * as __fd_glob_23 from "../content/docs/guides/troubleshooting.mdx?collection=docs"
+import * as __fd_glob_22 from "../content/docs/guides/text-vs-children.mdx?collection=docs"
+import * as __fd_glob_21 from "../content/docs/guides/react-use-shine.mdx?collection=docs"
+import * as __fd_glob_20 from "../content/docs/guides/performance-and-lifecycle.mdx?collection=docs"
+import * as __fd_glob_19 from "../content/docs/guides/light-position-and-intensity.mdx?collection=docs"
+import * as __fd_glob_18 from "../content/docs/guides/dynamic-updates.mdx?collection=docs"
+import * as __fd_glob_17 from "../content/docs/guides/class-api.mdx?collection=docs"
+import * as __fd_glob_16 from "../content/docs/getting-started/vanilla-quick-start.mdx?collection=docs"
+import * as __fd_glob_15 from "../content/docs/getting-started/react-quick-start.mdx?collection=docs"
+import * as __fd_glob_14 from "../content/docs/getting-started/nextjs-client-components.mdx?collection=docs"
+import * as __fd_glob_13 from "../content/docs/getting-started/installation.mdx?collection=docs"
+import * as __fd_glob_12 from "../content/docs/examples/shadow-rgb-cookbook.mdx?collection=docs"
+import * as __fd_glob_11 from "../content/docs/examples/playground.mdx?collection=docs"
 import * as __fd_glob_10 from "../content/docs/examples/mouse-follow.mdx?collection=docs"
 import * as __fd_glob_9 from "../content/docs/examples/direct-class-usage.mdx?collection=docs"
 import * as __fd_glob_8 from "../content/docs/examples/change-content.mdx?collection=docs"
@@ -30,4 +31,4 @@ const create = server<typeof Config, import("fumadocs-mdx/runtime/types").Intern
   }
 }>({"doc":{"passthroughs":["extractedReferences"]}});
 
-export const docs = await create.docs("docs", "content/docs", {"meta.json": __fd_glob_0, }, {"index.mdx": __fd_glob_1, "api/shine-class.mdx": __fd_glob_2, "api/shine-config-settings.mdx": __fd_glob_3, "api/shine-options.mdx": __fd_glob_4, "api/use-shine-hook.mdx": __fd_glob_5, "examples/auto-pilot.mdx": __fd_glob_6, "examples/box-shadow-children.mdx": __fd_glob_7, "examples/change-content.mdx": __fd_glob_8, "examples/direct-class-usage.mdx": __fd_glob_9, "examples/mouse-follow.mdx": __fd_glob_10, "examples/shadow-rgb-cookbook.mdx": __fd_glob_11, "getting-started/installation.mdx": __fd_glob_12, "getting-started/nextjs-client-components.mdx": __fd_glob_13, "getting-started/react-quick-start.mdx": __fd_glob_14, "getting-started/vanilla-quick-start.mdx": __fd_glob_15, "guides/class-api.mdx": __fd_glob_16, "guides/dynamic-updates.mdx": __fd_glob_17, "guides/light-position-and-intensity.mdx": __fd_glob_18, "guides/performance-and-lifecycle.mdx": __fd_glob_19, "guides/react-use-shine.mdx": __fd_glob_20, "guides/text-vs-children.mdx": __fd_glob_21, "guides/troubleshooting.mdx": __fd_glob_22, });
+export const docs = await create.docs("docs", "content/docs", {"meta.json": __fd_glob_0, }, {"index.mdx": __fd_glob_1, "api/shine-class.mdx": __fd_glob_2, "api/shine-config-settings.mdx": __fd_glob_3, "api/shine-options.mdx": __fd_glob_4, "api/use-shine-hook.mdx": __fd_glob_5, "examples/auto-pilot.mdx": __fd_glob_6, "examples/box-shadow-children.mdx": __fd_glob_7, "examples/change-content.mdx": __fd_glob_8, "examples/direct-class-usage.mdx": __fd_glob_9, "examples/mouse-follow.mdx": __fd_glob_10, "examples/playground.mdx": __fd_glob_11, "examples/shadow-rgb-cookbook.mdx": __fd_glob_12, "getting-started/installation.mdx": __fd_glob_13, "getting-started/nextjs-client-components.mdx": __fd_glob_14, "getting-started/react-quick-start.mdx": __fd_glob_15, "getting-started/vanilla-quick-start.mdx": __fd_glob_16, "guides/class-api.mdx": __fd_glob_17, "guides/dynamic-updates.mdx": __fd_glob_18, "guides/light-position-and-intensity.mdx": __fd_glob_19, "guides/performance-and-lifecycle.mdx": __fd_glob_20, "guides/react-use-shine.mdx": __fd_glob_21, "guides/text-vs-children.mdx": __fd_glob_22, "guides/troubleshooting.mdx": __fd_glob_23, });
